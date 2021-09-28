82b3b319326bbd13da0a1236777fc507d650af44
unknown <arokai1233@mail.ru>
2021-09-28 18:23:15 +0300

    Добавил комментарии для пояснения

--- src/strings/Strings.java
+++ src/strings/Strings.java
@@ -11,20 +11,22 @@ public class Strings {
 
     
     public static void main(String[] args) {
-        String text = "The quick brown fox jumps over the lazy dog";
-        int count = 0;
+        String text = "The quick brown fox jumps over the lazy dog"; //Переменная строка text, только состоящая из символов, неизменяемая.
+        int count = 0; //Переменная для хранения числа букв алфавита
         
+        //Преобразование строки в массив символов.
         char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y', 'z'};
         
-        for (int x = 0; x < letters.length; x++){
-            for(int i = 0; i < text.length();i++){
-                if(letters[x] == text.charAt(i)){
-                    count++;
+        for (int x = 0; x < letters.length; x++){ //Индекс массива - x, обьектов в массиве 26. Идёт по значением массива и программа начинает следующий цикл for.
+            for(int i = 0; i < text.length();i++){ //Цикл проверяет содержится ли данный символ из массива letters в строке text
+                if(letters[x] == text.charAt(i)){ //Значение массива (символ) совпадает со символом в текстe, то возвращает символ, находящийся по указанному индексу в строке.
+                    count++; //Если всё по условию соблюдается, то к переменной прибавляется 1. (И так до того как ячейки в массиве не закончатся их 26)
                     break;
                 }
             }
             
         }
+        //Если переменная имеет значение 26, тоесть в строке есть все буквы алфавита, выполняется следующее.
         if (count == 26){
             System.out.println("Строка содержит все буквы Английского алфавита");
         }
